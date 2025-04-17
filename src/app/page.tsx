05824d97484a700c18f4d6950d8884a94ca3d61a
rouf0919 <rouf0919@gmail.com>
2025-04-17 17:58:36 +0500

    change added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-// app/page.tsx
+import Link from "next/link";
 export default function HomePage() {
   return (
-    <div>
+    <div className="flex flex-col items-center gap-5 max-w-7xl mx-auto text-center py-4 px-4 sm:px-6 lg:px-8 home-page justify-center">
       <h1>Welcome to the Homepage</h1>
-      {/* You can link to /product/[id] or /cart here */}
+      <Link
+        href="/product"
+        className="
+        block bg-indigo-600 text-white text-center p-3 min-w-[200px] rounded-md
+        hover:bg-indigo-700 transition-colors duration-200"
+      >
+        Go To Product Page
+      </Link>
     </div>
   );
 }
